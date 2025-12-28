b5e6e7f9157dc51b6762671e1dec1d61452efce4
CodewithAli45 <ali.my0304@gmail.com>
2025-12-28 13:57:57 +0530

    backend deploy

--- server/config/app.js
+++ server/config/app.js
@@ -5,7 +5,9 @@ const songRoutes = require("../routes/songs");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 app.get("/", (req, res) => {
